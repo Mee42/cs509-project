a38dea06b0954f8f4ebb716ec91f6e592f74c44e
Chinh Dang <61719308+ChinhDangg@users.noreply.github.com>
2024-02-05 22:15:47 -0500

    initial backend files
    Just the initial backend files

---
+++ backend/src/test/java/cs509/backend/BackendApplicationTests.java
@@ -0,0 +1,13 @@
+package cs509.backend;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class BackendApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
