edf1095e2367f3a6b51b7cd424500488cb057457
Carson Graham <carson42g@gmail.com>
2024-03-12 17:59:05 -0400

    got tests working, initially 7% line coverage

--- backend/src/test/java/cs509/backend/BackendApplicationTests.java
+++ backend/src/test/java/cs509/backend/BackendApplicationTests.java
@@ -1,15 +1,40 @@
 package cs509.backend;
 
-import cs509.backend.Service.FlightService;
+import cs509.backend.Controller.FlightController;
 import org.junit.jupiter.api.Test;
+import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.boot.test.context.SpringBootTest;
+
+import java.util.Arrays;
+import java.util.HashSet;
+import java.util.List;
+
+import static org.assertj.core.api.AssertionsForClassTypes.*;
 
 @SpringBootTest
 class BackendApplicationTests {
 
+
+	@Autowired
+	FlightController flightController;
+
 	@Test
 	void contextLoads() {
+		assertThat(flightController).isNotNull();
+	}
 
+	@Test
+	void allArriveAirportsFollowStringFormat() {
+		List<String> airports = Arrays.stream(flightController.getAllArriveAirports()).toList();
+		for(var airport : airports) {
+			assertThat(airport).matches(".*?\\(.*?\\)");
+		}
+	}
+
+	@Test
+	void getAllArriveAirportsReturnsNoDuplicates() {
+		List<String> airports = Arrays.stream(flightController.getAllArriveAirports()).toList();
+		assertThat(new HashSet<>(airports).size()).isEqualTo(airports.size());
 	}
 
 }
