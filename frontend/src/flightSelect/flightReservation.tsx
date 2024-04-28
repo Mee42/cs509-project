3d8a7e19563250dea933fa85f42eaa9410572b1f
Travis Mann <tmann1794@gmail.com>
2024-04-28 14:20:06 -0400

    Added message for processes exceeding 3 seconds

--- frontend/src/flightSelect/flightReservation.tsx
+++ frontend/src/flightSelect/flightReservation.tsx
@@ -1,17 +1,36 @@
 import axios from "axios";
 import { submitReservationEndpoint } from "../endpoints";
 import { Flight } from "../model/flight";
+import { toast } from "react-toastify";
 
-export async function submitReservation(trip: Flight[], successFunc: CallableFunction, errorFunc: CallableFunction) {
+export async function submitReservation(
+  trip: Flight[],
+  successFunc: CallableFunction,
+  errorFunc: CallableFunction
+) {
+  let tripsReserved = false;
+
+  new Promise((r) => setTimeout(r, 3000)).then(() => {
+    if (!tripsReserved) {
+      toast("System is working...");
+    }
+  });
+
   return await axios
-    .post(submitReservationEndpoint, {
-      flights: trip,
-    })
+    .post(
+      submitReservationEndpoint,
+      {
+        flights: trip,
+      },
+      { timeout: 10000 }
+    )
     .then((response) => {
+      tripsReserved = true;
       console.log(response);
       successFunc();
     })
     .catch((error) => {
+      tripsReserved = true;
       console.error(`failed to submit reservation: ${error}`);
       errorFunc();
     });
